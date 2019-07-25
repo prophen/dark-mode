f083cbe78a51a8f5ca39e244041207eaf97af251
Nikema Prophet <nikema@gmail.com>
2019-07-25 15:41:59 -0700

    Creating useLocalStorage.js

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function useLocalStorage(key, initialValue) {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+  return [storedValue];
+}
+
+function setValue(value) {
+  setStoredValue(value);
+  window.localStorage.setItem(key, JSON.stringify(value));
+}
